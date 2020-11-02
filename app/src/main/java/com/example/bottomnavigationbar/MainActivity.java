3d2a8d5c6aa38dafef6827d4d69fe76d3f7c0e8a
CFD Joshuas <CFD08Joshua@gmail.com>
2020-11-02 11:07:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationbar/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.bottomnavigationbar;
+
+import androidx.annotation.ColorRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+/**
+ * @author CFD_08
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final String SELECTED_ITEM = "arg_selected_item";
+    ViewPager viewPager;
+    BottomNavigationView bottomNavigationView;
+    FragmentPagerAdapter fragmentPagerAdapter;
+    private int mSelectedItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return true;
+            }
+        });
+        MenuItem selectedItem;
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = bottomNavigationView.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = bottomNavigationView.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        MenuItem homeItem = bottomNavigationView.getMenu().getItem(0);
+        if (mSelectedItem != homeItem.getItemId()) {
+            // select home item
+            selectFragment(homeItem);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                frag = MenuFragment.newInstance(getString(R.string.text_home),
+                        getColorFromRes(R.color.color_home));
+                break;
+            case R.id.menu_notifications:
+                frag = MenuFragment.newInstance(getString(R.string.text_notifications),
+                        getColorFromRes(R.color.color_notifications));
+                break;
+            case R.id.menu_search:
+                frag = MenuFragment.newInstance(getString(R.string.text_search),
+                        getColorFromRes(R.color.color_search));
+                break;
+            default:
+                break;
+        }
+
+        // update selected item
+        mSelectedItem = item.getItemId();
+
+        // uncheck the other items.
+        for (int i = 0; i< bottomNavigationView.getMenu().size(); i++) {
+            MenuItem menuItem = bottomNavigationView.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
+
+        updateToolbarText(item.getTitle());
+
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, frag, frag.getTag());
+            ft.commit();
+        }
+    }
+
+    private void updateToolbarText(CharSequence text) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(text);
+        }
+    }
+
+    private int getColorFromRes(@ColorRes int resId) {
+        return ContextCompat.getColor(this, resId);
+    }
+}
